f1bd8529ca5a1de706c9d30bccda30b93ec46f8f
Michael Olbrich <m.olbrich@pengutronix.de>
2024-12-11 17:13:11 +0100

    split out mounting API filesystems and the new rootfs
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0-only
 use cmdline::{parse_cmdline, CmdlineOptions};
-use nix::mount::{mount, MsFlags};
+use mount::{mount_move_special, mount_root, mount_special};
 use nix::sys::termios::tcdrain;
 use nix::unistd::{chdir, chroot, dup2, execv, unlink};
 use std::env::current_exe;
 use std::ffi::CString;
-use std::fs::{create_dir, read_to_string, remove_dir, OpenOptions};
+use std::fs::{read_to_string, OpenOptions};
 use std::io;
 use std::os::fd::{AsFd, AsRawFd, RawFd};
-use std::path::Path;
 use usbg_9pfs::prepare_9pfs_gadget;
 
 mod cmdline;
+mod mount;
 mod usbg_9pfs;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -34,84 +34,8 @@ fn setup_console() -> Result<()> {
     Ok(())
 }
 
-fn mount_apivfs(dst: &str, fstype: &str) -> Result<()> {
-    if let Err(e) = create_dir(dst) {
-        if e.kind() != io::ErrorKind::AlreadyExists {
-            return Err(format!("Failed to create {dst}: {e}").into());
-        }
-    }
-
-    mount(
-        Some(Path::new(fstype)),
-        Path::new(dst),
-        Some(Path::new(fstype)),
-        MsFlags::empty(),
-        Option::<&Path>::None,
-    )
-    .map_err(|e| format!("Failed to mount {fstype} -> {dst}: {e}"))?;
-
-    Ok(())
-}
-
-fn mount_root(
-    src: Option<String>,
-    fstype: Option<String>,
-    flags: MsFlags,
-    args: Option<String>,
-) -> Result<()> {
-    if let Err(e) = create_dir("/root") {
-        if e.kind() != io::ErrorKind::AlreadyExists {
-            return Err(format!("Failed to create /root: {e}").into());
-        }
-    }
-
-    println!(
-        "Mounting rootfs {} -> /root ({}, '{}')",
-        src.as_deref().unwrap(),
-        fstype.as_deref().unwrap_or_default(),
-        args.as_deref().unwrap_or_default()
-    );
-    mount(
-        src.as_deref(),
-        Path::new("/root"),
-        fstype.as_deref(),
-        flags,
-        args.as_deref(),
-    )
-    .map_err(|e| {
-        format!(
-            "Failed to mount {} -> /root ({}, '{}'): {e}",
-            src.unwrap(),
-            fstype.unwrap_or_default(),
-            args.unwrap_or_default()
-        )
-    })?;
-
-    Ok(())
-}
-
-fn mount_move(src: &str, dst: &str) -> Result<()> {
-    mount(
-        Some(Path::new(src)),
-        dst,
-        Option::<&Path>::None,
-        MsFlags::MS_MOVE,
-        Option::<&Path>::None,
-    )
-    .map_err(|e| format!("Failed to move mount {src} -> {dst}: {e}"))?;
-
-    remove_dir(src)?;
-
-    Ok(())
-}
-
-fn mount_special() -> Result<()> {
-    mount_apivfs("/dev", "devtmpfs")?;
-    mount_apivfs("/sys", "sysfs")?;
-    if Path::new("/sys/kernel/config").is_dir() {
-        mount_apivfs("/sys/kernel/config", "configfs")?;
-    }
-    mount_apivfs("/proc", "proc")?;
+fn run() -> Result<()> {
+    mount_special(true)?;
 
     let cmdline = read_to_string("/proc/cmdline")
         .map_err(|e| format!("Failed to read /proc/cmdline: {e}"))?;
@@ -122,33 +46,14 @@ fn mount_special() -> Result<()> {
 
     prepare_9pfs_gadget(&options)?;
 
-    if options.root.is_none() {
-        return Err("root= not found in /proc/cmdline".into());
-    }
-    mount_root(
-        options.root,
-        options.rootfstype,
-        options.rootfsflags,
-        options.rootflags,
-    )?;
+    mount_root(&options)?;
 
     match current_exe() {
         Err(e) => println!("current_exe failed: {e}"),
         Ok(exe) => unlink(exe.as_path())?,
     }
 
-    mount_move("/dev", "/root/dev")?;
-    mount_move("/sys", "/root/sys")?;
-    mount_move("/proc", "/root/proc")?;
-
-    mount(
-        Some(Path::new("/")),
-        "/root/mnt",
-        Option::<&Path>::None,
-        MsFlags::MS_BIND,
-        Option::<&Path>::None,
-    )
-    .map_err(|e| format!("Failed to bind mount / -> /root/mnt: {e}"))?;
+    mount_move_special()?;
 
     chdir("/root")?;
     chroot(".")?;
@@ -168,7 +73,7 @@ fn main() -> Result<()> {
 
     println!("Running init...");
 
-    if let Err(e) = mount_special() {
+    if let Err(e) = run() {
         println!("{e}");
     }
     /* Make sure all output is written before exiting */
---
+++ src/mount.rs
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: GPL-2.0-only
+use crate::cmdline::CmdlineOptions;
+use nix::mount::{mount, MsFlags};
+use std::fs::{create_dir, remove_dir};
+use std::io;
+use std::path::Path;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn mount_apivfs(dst: &str, fstype: &str) -> Result<()> {
+    if let Err(e) = create_dir(dst) {
+        if e.kind() != io::ErrorKind::AlreadyExists {
+            return Err(format!("Failed to create {dst}: {e}").into());
+        }
+    }
+
+    mount(
+        Some(Path::new(fstype)),
+        Path::new(dst),
+        Some(Path::new(fstype)),
+        MsFlags::empty(),
+        Option::<&Path>::None,
+    )
+    .map_err(|e| format!("Failed to mount {fstype} -> {dst}: {e}"))?;
+
+    Ok(())
+}
+
+fn mount_rootfs(
+    src: &Option<String>,
+    fstype: &Option<String>,
+    flags: MsFlags,
+    args: &Option<String>,
+) -> Result<()> {
+    if let Err(e) = create_dir("/root") {
+        if e.kind() != io::ErrorKind::AlreadyExists {
+            return Err(format!("Failed to create /root: {e}").into());
+        }
+    }
+
+    println!(
+        "Mounting rootfs {} -> /root ({}, '{}')",
+        src.as_deref().unwrap(),
+        fstype.as_deref().unwrap_or_default(),
+        args.as_deref().unwrap_or_default()
+    );
+    mount(
+        src.as_deref(),
+        Path::new("/root"),
+        fstype.as_deref(),
+        flags,
+        args.as_deref(),
+    )
+    .map_err(|e| {
+        format!(
+            "Failed to mount {} -> /root ({}, '{}'): {e}",
+            src.clone().unwrap_or_default(),
+            fstype.clone().unwrap_or_default(),
+            args.clone().unwrap_or_default()
+        )
+    })?;
+
+    Ok(())
+}
+
+pub fn mount_root(options: &CmdlineOptions) -> Result<()> {
+    if options.root.is_none() {
+        return Err("root= not found in /proc/cmdline".into());
+    }
+    mount_rootfs(
+        &options.root,
+        &options.rootfstype,
+        options.rootfsflags,
+        &options.rootflags,
+    )?;
+    Ok(())
+}
+
+fn mount_move(src: &str, dst: &str) -> Result<()> {
+    mount(
+        Some(Path::new(src)),
+        dst,
+        Option::<&Path>::None,
+        MsFlags::MS_MOVE,
+        Option::<&Path>::None,
+    )
+    .map_err(|e| format!("Failed to move mount {src} -> {dst}: {e}"))?;
+
+    remove_dir(src)?;
+
+    Ok(())
+}
+
+pub fn mount_special(mount_config: bool) -> Result<()> {
+    mount_apivfs("/dev", "devtmpfs")?;
+    mount_apivfs("/sys", "sysfs")?;
+    if mount_config && Path::new("/sys/kernel/config").is_dir() {
+        mount_apivfs("/sys/kernel/config", "configfs")?;
+    }
+    mount_apivfs("/proc", "proc")?;
+    Ok(())
+}
+
+pub fn mount_move_special() -> Result<()> {
+    mount_move("/dev", "/root/dev")?;
+    mount_move("/sys", "/root/sys")?;
+    mount_move("/proc", "/root/proc")?;
+
+    mount(
+        Some(Path::new("/")),
+        "/root/mnt",
+        Option::<&Path>::None,
+        MsFlags::MS_BIND,
+        Option::<&Path>::None,
+    )
+    .map_err(|e| format!("Failed to bind mount / -> /root/mnt: {e}"))?;
+
+    Ok(())
+}
