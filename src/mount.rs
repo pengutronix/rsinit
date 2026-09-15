cbae56a09ca7b257eb3fc9faf567b18a0d29de27
Stefan Kerkmann <s.kerkmann@pengutronix.de>
2026-09-15 15:17:10 +0200

    mount: remove stray ')' in error message
    Signed-off-by: Stefan Kerkmann <s.kerkmann@pengutronix.de>

--- src/mount.rs
+++ src/mount.rs
@@ -23,7 +23,7 @@ pub fn do_mount(
 
     mount(src, dst, fstype, flags, data).map_err(|e| {
         format!(
-            "Failed to mount {} -> {} as '{}' with flags = {:#x}, data = '{}'): {e}",
+            "Failed to mount {} -> {} as '{}' with flags = {:#x}, data = '{}': {e}",
             src.unwrap_or_default(),
             dst,
             fstype.unwrap_or_default(),
